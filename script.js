67b1cf0170a7e7bb41da62e924b7d9f9e4e2e8a6
laortizt <adri26.ortiz@gmail.com>
2021-02-10 14:26:32 -0500

    ok

---
+++ script.js
@@ -0,0 +1,9 @@
+/* todo el script*/ 
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
